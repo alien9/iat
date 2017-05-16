d79df888f24f8bc75ed477d6db8948193a7b098b
Tiago Barufi <barufi@gmail.com>
2017-05-16 18:24:42 -0300

    desenhando a handler

---
+++ app/src/main/java/br/com/homembala/dedos/util/Lever.java
@@ -0,0 +1,30 @@
+package br.com.homembala.dedos.util;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import br.com.homembala.dedos.R;
+
+/**
+ * Created by tiago on 16/05/17.
+ */
+
+public class Lever extends View {
+    private void init() {
+        setBackground(getResources().getDrawable(R.drawable.circle,null));
+    }
+    public Lever(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+    public Lever(Context context, @Nullable AttributeSet attrs){
+        super(context, attrs);
+        init();
+    }
+    public Lever(Context context) {
+        super(context);
+        init();
+    }
+}
